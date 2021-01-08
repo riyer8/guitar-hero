96a03262be876f937fd4eaf3421f02a8de86e2ff
Ramya Iyer <ramyaiyer1000@gmail.com>
2021-01-08 09:00:32 -0600

    Update StdDraw.java

--- StdDraw.java
+++ StdDraw.java
@@ -1,4 +1,4 @@
-// Copyright 2000–2011, Robert Sedgewick and Kevin Wayne.
+// Copyright 2000â€“2011, Robert Sedgewick and Kevin Wayne.
 
 /*************************************************************************
  *  Compilation:  javac StdDraw.java
@@ -206,7 +206,7 @@ public final class StdDraw implements ActionListener, MouseListener, MouseMotion
     // create the menu bar (changed to private)
     private static JMenuBar createMenuBar() {
         JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("Made by Ramya Iyer");
+        JMenu menu = new JMenu("Made by Ramya Iyer and Jewell Jordan");
         menuBar.add(menu);
         JMenuItem menuItem1 = new JMenuItem(" Save...   ");
         menuItem1.addActionListener(std);
